1b411b3c5cf32d4da68a351fc8ca22a46acf66af
unknown <alazim-star>
2024-12-26 17:58:32 +0600

    add banner

--- src/Components/Header/Banner.jsx
+++ src/Components/Header/Banner.jsx
@@ -84,7 +84,7 @@ const Banner = () => {
           </p>
         </div>
       </div>
-      {/* Close Button */}
+      {/*modal Close Button */}
       <button
         className="absolute top-2 right-2 text-gray-500  z-20"
         onClick={closeModal}
